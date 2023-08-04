5845d224ecaa93ae3fada3e082f9cfd3b87d9415
AliSuliman <ali.sliman@ramaaz.com>
2023-08-05 02:06:48 +0300

    - fix store order.

--- app/Modules/Orders/Actions/StoreOrderAction.php
+++ app/Modules/Orders/Actions/StoreOrderAction.php
@@ -10,7 +10,14 @@ class StoreOrderAction
     public static function execute(
     OrderDTO $orderDTO
     ){
+        Order::query()
+            ->where('table_id', $orderDTO->table_id)
+            ->where('status', Order::ACTIVE)
+            ->update([
+                'status' => Order::EXPIRED
+            ]);
 
+        $orderDTO->status = Order::ACTIVE;
         return Order::create(array_null_filter($orderDTO->toArray()));
     }
 }
